a4986bb2ae973401c4b5745c667c94a8707b82c4
melias198 <melias4600@gmail.com>
2023-06-25 19:38:36 +0600

    Day 1

--- Week 04/Day 1/boxes.cpp
+++ Week 04/Day 1/boxes.cpp
@@ -18,7 +18,30 @@ using namespace std;
 
 void solve()
 {
-    
+    ll n;
+    cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if(a[i]&1) a[i]--;
+    }
+    sort(a,a+n);
+    ll mx=0;
+    map<ll,ll>mp;
+    for(ll i=0;i<n;i++)
+    {
+        ll ans=0;
+        while(a[i]>1)
+        {
+            ans++;
+            a[i]/=2;
+        }
+        mp[ans]++;
+        mx=max(mx,ans);
+    }
+    ll final=ceil(mp[mx]*1.0/2);
+    cout<<final<<'\n';
 }
 
 int main()
--- Week 04/Day 1/possible_or_not.cpp
+++ Week 04/Day 1/possible_or_not.cpp
@@ -18,7 +18,33 @@ using namespace std;
 
 void solve()
 {
-    
+    ll n,b;
+    cin>>n>>b;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        if((x&b)==b) v.push_back(x);
+    }
+
+    if(v.size()==0) cout<<"NO"<<'\n';
+    else
+    {
+        int a=v[0];
+        for(int i=1;i<v.size();i++)
+        {
+            a=a&v[i];
+        }
+        if(a==b)
+        {
+            cout<<"YES"<<'\n';
+        }
+        else
+        {
+            cout<<"NO"<<'\n';
+        }
+    }
 }
 
 int main()
