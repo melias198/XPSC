0cc823ffaff79240a11c443bae6273445753620c
melias198 <melias4600@gmail.com>
2023-06-27 12:46:14 +0600

    Day 5

---
+++ Week 04/Day 5/two_elevators.cpp
@@ -0,0 +1,41 @@
+/*
+||-----------------------------------||
+||           Mohammad Elias          ||
+||-----------------------------------||
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define PI 3.14159265359
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define cyes cout<<"Yes"<<endl
+#define cno cout<<"No"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+
+void solve()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    int one=a-1;
+    int two = abs(b-c)+(c-1);
+    
+    if(one<two) cout<<1<<'\n';
+    else if(two<one) cout<<2<<'\n';
+    else cout<<3<<'\n';
+}
+
+int main()
+{
+    fast;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
