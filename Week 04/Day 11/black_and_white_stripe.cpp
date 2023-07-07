9d12d32d917d825deb6e8eaf7c158e826387bedc
melias198 <melias4600@gmail.com>
2023-07-07 14:16:39 +0600

    Day 11

--- Week 04/Day 11/black_and_white_stripe.cpp
+++ Week 04/Day 11/black_and_white_stripe.cpp
@@ -18,7 +18,22 @@ using namespace std;
 
 void solve()
 {
-    
+     int n, k;
+     cin >> n >> k;
+     string s;
+     cin >> s;
+     vector<int> w(n + 1);
+     for (int i = 1; i <= n; i++)
+     {
+        w[i] = w[i - 1] + int(s[i - 1] == 'W');
+     }
+        
+     int result = INT_MAX;
+     for (int i = k; i <= n; i++)
+     {
+        result = min(result, w[i] - w[i - k]);
+     }
+     cout << result << endl;
 }
 
 int main()
