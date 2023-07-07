cbef53fa9f2ddcf325a7de252b0f8b2e62867b0d
melias198 <melias4600@gmail.com>
2023-07-07 14:10:52 +0600

    Day 9

--- Week 04/Day 9/print_a_pedestal.cpp
+++ Week 04/Day 9/print_a_pedestal.cpp
@@ -20,7 +20,21 @@ void solve()
 {
     int n;
     cin>>n;
-    
+    for(int a = 3; a < n; a++) 
+    {
+        int c = (n - a) / 2;
+        int b = n - a - c;
+        if (c > 1 && b+1 < a) 
+        {
+            c--;
+            b++;
+        }
+        if (a > b && b > c) 
+        {
+            cout << b << ' ' << a << ' ' << c << endl;
+            return;
+        }
+    }
 }
 
 int main()
