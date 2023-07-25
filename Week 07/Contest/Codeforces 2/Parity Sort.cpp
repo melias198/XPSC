5c2f3935de8ede9a407b5bfad563e20415cdf5db
melias198 <melias4600@gmail.com>
2023-07-25 23:49:13 +0600

    Contest

---
+++ Week 07/Contest/Codeforces 2/Parity Sort.cpp
@@ -0,0 +1,56 @@
+/*
+||-----------------------------------||
+||           Mohammad Elias          ||
+||-----------------------------------||
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define PI 3.14159265359
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define cyes cout<<"Yes"<<endl
+#define cno cout<<"No"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n),tmp(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+
+    tmp=v;
+    sort(v.begin(),v.end());
+    
+    bool flag=true;
+    for(int i=0;i<n;i++)
+    {
+        if((v[i]&1)!=(tmp[i]&1))
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag) cout<<"YES"<<'\n';
+    else cout<<"NO"<<'\n';
+}
+
+int main()
+{
+    fast;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
