df131de600d8aea9a5b4d7ee968cbfc47954523e
melias198 <melias4600@gmail.com>
2023-08-05 19:45:18 +0600

    Day 1

--- Week 09/Day 1/Greg and Array.cpp
+++ Week 09/Day 1/Greg and Array.cpp
@@ -9,13 +9,56 @@ int main()
 
     ll n,m,k;
     cin>>n>>m>>k;
-    vector<ll>v(n);
-    for(int i=0;i<n;i++)
+
+    vector<ll>v(n+1);
+    for(int i=1;i<=n;i++)
     {
         cin>>v[i];
     }
 
-    
+    vector<pair<int,int>>range(m+1);
+    vector<ll>value(m+1);
+    for(int i=1;i<=m;i++)
+    {
+        int l,r,d;
+        cin>>l>>r>>d;
+        range[i]={l,r};
+        value[i]=d;
+    }
+
+    ll prefix[m+1]={0};
+    for(int i=1;i<=k;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        prefix[x]++;
+        prefix[y+1]--;
+    }
+
+    ll sum=0;
+    for(int i=1;i<=m;i++)
+    {
+        sum+=prefix[i];
+        value[i]=value[i]*sum;
+    }
+
+    ll ans[n+1]={0};
+    for(int i=1;i<=m;i++)
+    {
+        int a=range[i].first;
+        int b=range[i].second;
+
+        ans[a]+=value[i];
+        ans[b+1]-=value[i];
+    }
+
+    ll tmp=0;
+    for(int i=1;i<=n;i++)
+    {
+        tmp+=ans[i];
+        cout<<tmp+v[i]<<" ";
+    }
+    cout<<'\n';
 
     return 0;
 }
