0e33504037468270efd1b718927cd875c8a6e9ee
melias198 <melias4600@gmail.com>
2023-09-08 21:12:36 +0600

    Day 1

---
+++ Week 14/Day 1/The Corridor or There and Back Again.cpp
@@ -0,0 +1,49 @@
+/*
+||-----------------------------------||
+||           Mohammad Elias          ||
+||-----------------------------------||
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define PI 3.14159265359
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define cyes cout<<"Yes"<<endl
+#define cno cout<<"No"<<endl
+#define minus cout<<-1<<endl
+using namespace std;
+
+void solve()
+{
+   int n;
+   cin>>n;
+   vector<int>d(n),s(n);
+   for(int i=0;i<n;i++)
+   {
+        cin>>d[i]>>s[i];
+   }
+
+   int ans=INT_MAX;
+   for(int i=0;i<n;i++)
+   {
+        int x=(s[i]-1)/2;
+        ans=min(ans,x+d[i]);
+   }
+
+   cout<<ans<<'\n';
+}
+
+int main()
+{
+    fast;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
