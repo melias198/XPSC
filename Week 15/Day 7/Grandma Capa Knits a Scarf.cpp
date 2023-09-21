99a5fee240d479104714ed49dc74691b2e5fdd81
melias198 <melias4600@gmail.com>
2023-09-21 23:32:10 +0600

    Day 7

--- Week 15/Day 7/Grandma Capa Knits a Scarf.cpp
+++ Week 15/Day 7/Grandma Capa Knits a Scarf.cpp
@@ -15,37 +15,20 @@
 #define cno cout<<"No"<<endl
 #define minus cout<<-1<<endl
 using namespace std;
-ll n;
-vector<ll>v;
 
 void solve()
 {
-   cin>>n;
-   v.resize(n);
-
-   for(int i=0;i<n;i++) cin>>v[i];
-   sort(v.begin(),v.end());
-
-   ll q;
-   cin>>q;
-   while(q--)
-   {
-        ll l,r;
-        cin>>l>>r;
-        auto it1=upper_bound(v.begin(),v.end(),r);
-        auto it2=lower_bound(v.begin(),v.end(),l);
-        cout<<it1-it2<<" ";
-   }
+   
 }
 
 int main()
 {
     fast;
-   // ll t;
-   // cin >> t;
-   // while (t--)
-   // {
+    ll t;
+    cin >> t;
+    while (t--)
+    {
         solve();
-   // }
+    }
     return 0;
 }
